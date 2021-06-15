c4d143c8f0522c5d9454e3f2bd83f1ab800a8a3f
pedroprosdocimi <pedro@DESKTOP-FH9M3UH>
2021-06-14 22:56:10 -0300

    Creating project and solution.

---
+++ StockQuoteAlert/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StockQuoteAlert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
