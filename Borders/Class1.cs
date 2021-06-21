5dda4ea16bd9a493272f97fbe7549c6bbb83a89b
pedroprosdocimi <pedro@DESKTOP-FH9M3UH>
2021-06-20 23:17:44 -0300

    Adding project structure Api/UseCases/Repositories/Borders.

---
+++ Borders/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Borders
+{
+    public class Class1
+    {
+    }
+}
---
+++ StockQuoteAlert/Config/AppConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+
+namespace StockQuoteAlert.Config
+{
+    public class AppConfig
+    {
+        public IEnumerable<string> Emails { get; set; }
+        public object SMTPConfiguration { get; init; }
+
+        private void ReadAllSetting()
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+
+                Emails = appSettings["Emails"].Split(",").ToList();
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings.");
+            }
+        }
+    }
+}
