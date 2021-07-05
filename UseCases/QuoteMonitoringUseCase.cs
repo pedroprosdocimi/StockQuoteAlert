07d29e0361e7055bfabd4f4f57068f17f77ba4e4
pedroprosdocimi <pedro@DESKTOP-FH9M3UH>
2021-07-05 15:32:39 -0300

    Creating quote monitoring use case and updating solution.

---
+++ UseCases/QuoteMonitoringUseCase.cs
@@ -0,0 +1,51 @@
+﻿using Borders.Constants;
+using Borders.Dtos;
+using Borders.Repositories;
+using Borders.UseCases;
+using Borders.Validators;
+using FluentValidation;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace UseCases
+{
+    public class QuoteMonitoringUseCase : IQuoteMonitoringUseCase
+    {
+        private readonly IStockQuotationRepository stockQuotationRepository;
+        private readonly IEmailRepository emailRepository;
+        private readonly ILogger<QuoteMonitoringUseCase> logger;
+
+        public QuoteMonitoringUseCase(IStockQuotationRepository stockQuotationRepository, IEmailRepository emailRepository, 
+            ILogger<QuoteMonitoringUseCase> logger)
+        {
+            this.stockQuotationRepository = stockQuotationRepository;
+            this.emailRepository = emailRepository;
+            this.logger = logger;
+        }
+
+        public async Task Execute(QuoteMonitoringRequest request)
+        {
+            
+
+            var stockSymbol = request.StockSymbol;
+            var stockQuotation = await stockQuotationRepository.Get(stockSymbol);
+            var actualStockQuotation = stockQuotation.Price;
+
+            if (actualStockQuotation > request.MaxValue)
+            {
+                emailRepository.Post(
+                    subject: EmailConstants.StockQuoteEmailSubject,
+                    html: string.Format(EmailConstants.StockQuoteEmailBody, stockSymbol, actualStockQuotation, "sell"));
+            }
+            else if (actualStockQuotation < request.MinValue)
+            {
+                emailRepository.Post(
+                    subject: EmailConstants.StockQuoteEmailSubject,
+                    html: string.Format(EmailConstants.StockQuoteEmailBody, stockSymbol, actualStockQuotation, "buy"));
+            }
+
+            logger.LogInformation("The Quote Monitoring Use Case finished successfully.");
+        }
+    }
+}
+
