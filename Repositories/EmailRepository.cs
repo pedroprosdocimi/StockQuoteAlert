aabf638d5d0d04f8cc3f9f9f852ab9119469d975
pedroprosdocimi <pedro@DESKTOP-FH9M3UH>
2021-07-05 15:30:13 -0300

    Creating email repository to send email and stock quotation repository to get the quote price.

---
+++ Repositories/EmailRepository.cs
@@ -0,0 +1,61 @@
+﻿using Borders.Configs;
+using Borders.Repositories;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Logging;
+using MimeKit;
+using MimeKit.Text;
+using System;
+
+namespace Repositories
+{
+    public class EmailRepository : IEmailRepository
+    {
+        private readonly ApplicationConfig applicationConfig;
+        private readonly ILogger logger;
+
+        public EmailRepository(ApplicationConfig applicationConfig, ILogger<EmailRepository> logger)
+        {
+            this.applicationConfig = applicationConfig;
+            this.logger = logger;
+        }
+
+        public void Post(string subject, string emailBody)
+        {
+            var receiverEmail = applicationConfig.SMTPConfiguration.EmailTo;
+            try
+            {
+                MimeMessage email = CreateEmail(receiverEmail, subject, emailBody);
+                SendEmail(email);             
+
+                logger.LogInformation($"Email successfully sent to {receiverEmail}.");
+            }
+            catch (Exception e)
+            {
+                logger.LogInformation($"An error occurred when trying to send email to {receiverEmail}. Error: {e.Message}");
+
+                throw new Exception($"Error when trying to send email to {receiverEmail}.");
+            }
+        }
+
+        private void SendEmail(MimeMessage email)
+        {
+            using var smtp = new SmtpClient();
+            smtp.Connect(applicationConfig.SMTPConfiguration.SmtpHost, applicationConfig.SMTPConfiguration.SmtpPort, SecureSocketOptions.StartTls);
+            smtp.Authenticate(applicationConfig.SMTPConfiguration.SmtpUser, applicationConfig.SMTPConfiguration.SmtpPass);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+
+        private MimeMessage CreateEmail(string receiverEmail, string subject, string emailBody)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(applicationConfig.SMTPConfiguration.EmailFrom));
+            email.To.Add(MailboxAddress.Parse(receiverEmail));
+            email.Subject = subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = emailBody };
+
+            return email;
+        }
+    }
+}
---
+++ Repositories/StockQuotationRepository.cs
@@ -0,0 +1,58 @@
+﻿using Borders.Configs;
+using Borders.Dtos;
+using Borders.Repositories;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class StockQuotationRepository : IStockQuotationRepository
+    {
+        private readonly HttpClient httpClient;
+        private readonly ApplicationConfig applicationConfig;
+        private readonly ILogger logger;
+
+        public StockQuotationRepository(HttpClient httpClient, ApplicationConfig applicationConfig, 
+            ILogger<StockQuotationRepository> logger)
+        {
+            this.httpClient = httpClient;
+            this.applicationConfig = applicationConfig;
+            this.logger = logger;
+        }
+
+        public async Task<StockResult> Get(string request)
+        {
+            using var httpResponse = await httpClient.GetAsync($"/finance/stock_price?key={applicationConfig.ApiKey}&symbol={request}");
+
+            if (!httpResponse.IsSuccessStatusCode)
+                throw new Exception($"Error when trying to get {request} price information. " +
+                    $"ErrorCode: {httpResponse.StatusCode}.");
+               
+            var jsonResponse = await httpResponse.Content.ReadAsStringAsync();
+            return GetStockQuoteInfo(jsonResponse, request);
+            
+                                  
+        }
+
+        private StockResult GetStockQuoteInfo(string jsonResponse, string request)
+        {
+            object resultObject;
+            object stocksInfoObject;
+
+            var resultDictionary = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonResponse);            
+            resultDictionary.TryGetValue("results", out resultObject);
+
+            var stocksInfoPayload = JsonConvert.SerializeObject(resultObject);
+            var stocksInfoDictionary = JsonConvert.DeserializeObject<Dictionary<string, object>>(stocksInfoPayload);            
+            stocksInfoDictionary.TryGetValue(request, out stocksInfoObject);
+
+            var requestedStockInfo = JsonConvert.SerializeObject(stocksInfoObject);
+            
+            return JsonConvert.DeserializeObject<StockResult>(requestedStockInfo);
+        }
+    }
+}
